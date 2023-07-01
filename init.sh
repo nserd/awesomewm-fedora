d353b62bf6dfec2cfd5b38fd5116298dc5200093
Nikita Serdyukov <n.serdyukov@just-ai.com>
2023-07-01 16:56:32 +0300

    cleanup

--- init.sh
+++ init.sh
@@ -5,5 +5,4 @@ git clone https://github.com/nserd/awesomewm-fedora && \
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
 python get-pip.py && \
 rm get-pip.py && \
-pip install ansible && \
-cd awesomewm-fedora
+pip install ansible
