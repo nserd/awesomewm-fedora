de74e2ca90d0c5ba8e44acf25689cefd156e294c
Nikita Serdyukov <serdiukovn@gmail.com>
2023-11-30 14:28:38 +0300

    add layout

--- roles/system/files/default_layout.sh
+++ roles/system/files/default_layout.sh
@@ -5,13 +5,18 @@ function one_monitor_layout {
     /usr/bin/xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output HDMI-2 --off
 }
 
-function two_monitors_layout {
+function two_monitors_layout_horizontal {
     /usr/bin/xrandr --listactivemonitors &> /dev/null
     /usr/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x644 --rotate normal --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+}
+
+function two_monitors_layout_vertical {
+    /usr/bin/xrandr --listactivemonitors &> /dev/null
+    /usr/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 }
 
 TRIGGER="$1"
 HDMI_STATUS="$(cat /sys/class/drm/card1-HDMI-A-2/status)"
 
-[ "$HDMI_STATUS" == "connected" ] && two_monitors_layout || one_monitor_layout
+[ "$HDMI_STATUS" == "connected" ] && two_monitors_layout_vertical || one_monitor_layout
 [ "$TRIGGER" != "awesome" ] && /usr/bin/pkill -HUP awesome
